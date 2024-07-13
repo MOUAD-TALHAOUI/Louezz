b39d89a9a66af462b8eb287b6a3a4689e8fe0b06
mouad Talhaoui <talhaouimouad04@gmail.com>
2024-07-13 19:53:07 +0100

    initilizing backend project

---
+++ Louezz-api/src/main/java/com/louezz/Louezz_api/LouezzApiApplication.java
@@ -0,0 +1,13 @@
+package com.louezz.Louezz_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouezzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouezzApiApplication.class, args);
+	}
+
+}
---
+++ Louezz-api/src/test/java/com/louezz/Louezz_api/LouezzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louezz.Louezz_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouezzApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
